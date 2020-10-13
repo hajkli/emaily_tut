505be1867cd8e4f2b5a46e422574692133e84b8e
hajkli <hajkli@gmail.com>
2020-10-13 11:40:22 +0200

    finish auth flow fix 1

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,7 @@
 //keys.js dont commit this
 //keys.js figuer out what set of credentials to return
 
-if (process.env.POR === 'production') {
+if (process.env.NODE_ENV === 'production') {
 	//we are in production return the prod set of keys
 	module.exports = require('./prod');
 } else {
