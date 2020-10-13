cfa58e38e1df283cde45b68efd17471528d88839
hajkli <hajkli@gmail.com>
2020-10-13 13:29:37 +0200

    finis

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,15 @@
 const passport = require('passport');
-
+//test
 module.exports = (app) => {
 	app.get(
 		'/auth/google',
 		passport.authenticate('google', {
 			scope: ['profile', 'email'],
+			prompt: 'select_account',
 		})
 	);
+
+	console.log('test');
 
 	app.get('/auth/google/callback', passport.authenticate('google'));
 	app.get('/api/current_user', (req, res) => {
